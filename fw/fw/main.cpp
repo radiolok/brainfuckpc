80403a7592131d66a6db822ed4f54339f0c98e88
Artem Kashkanov <ask@urlnn.ru>
2017-05-29 22:41:49 +0300

    Init Avr project

---
+++ fw/fw/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * fw.cpp
+ *
+ * Created: 29.05.2017 22:16:35
+ * Author : radiolok
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ fw/fw/util/filters.h
@@ -0,0 +1,24 @@
+/*Copyright (c) 2013, Artem Kashkanov
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
+
+
+#ifndef FILTERS_H_
+#define FILTERS_H_
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <inttypes.h>
+
+int16_t I16FIRFilter(int16_t sample_data, int16_t* _InputData, int16_t* _CoefData, int16_t* _OutputData, uint8_t _FilerRang);
+
+
+
+#endif /* FILTERS_H_ */
